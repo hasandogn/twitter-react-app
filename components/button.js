f18958ec8381577e28bdc9887bf991e69e263a25
Hasan <hsndogn80@gmail.com>
2021-01-22 01:36:47 +0300

    Updated navigation and styles files.

--- components/button.js
+++ components/button.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import cn from 'classnames'
 
 import style from './button.module.css'
 
-function Button ({children}) {
-    return <button type="button" className={style.button}>{children}</button>
+function Button({ children, className, ...props }) {
+  return (
+    <button type="button" className={cn(style.button, className)} {...props}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
