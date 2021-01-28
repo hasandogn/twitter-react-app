df80c25606f01327fdd94f1deb90265650bc705b
Hasan <hsndogn80@gmail.com>
2021-01-28 18:41:21 +0300

    Updated style and added button chance.

--- components/button.js
+++ components/button.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import cn from 'classnames'
+import Link from 'next/link'
 
-import style from './button.module.css'
+import styles from './button.module.css'
 
-function Button({ children, className, ...props }) {
+function LinkButton({ href, children, ...props }) {
   return (
-    <button type="button" className={cn(style.button, className)} {...props}>
+    <Link href={href}>
+      <a {...props}>{children}</a>
+    </Link>
+  )
+}
+
+function BaseButton({ children, ...props }) {
+  return (
+    <button type="button" {...props}>
       {children}
     </button>
+  )
+}
+
+function Button({ full = false, children, className, ...props }) {
+  const Comp = props.href ? LinkButton : BaseButton
+
+  return (
+    <Comp
+      className={cn(styles.button, full && styles.fullWidth, className)}
+      {...props}
+    >
+      {children}
+    </Comp>
   )
 }
 
