f94a02ca1f2c6149174854bab4f0c3041d83dfd6
Hasan <hsndogn80@gmail.com>
2021-01-30 12:07:34 +0300

    Added post component.

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,30 @@ import React from 'react'
 
 import useWindowSize from '../hooks/useWindowSize'
 import Layout from '../components/layout'
+import Tweet from '../components/tweet'
 
 function HomePage() {
-  return <Layout>Anasayfa</Layout>
+  return (
+    <Layout>
+      <Tweet
+        name="Hasan Dogan"
+        slug="premiumEfendi"
+        datetime={new Date('2021-01-30')}
+        text={`Hello my friend, I'm Hasan, I am a computer engineer.Hello my friend, I'm Hasan, I am a computer engineer.Hello my friend, I'm Hasan, I am a computer engineer.Hello my friend, I'm Hasan, I am a computer engineer.`}
+      />
+      <Tweet
+        name="Hasan Dogan"
+        slug="premiumEfendi"
+        datetime={new Date('2021-01-30')}
+        text={`hasan hasan hasan hasan hasan hasan 
+        
+        hasan hasan ...
+                hasan hasan ,..
+        `}
+      />
+
+    </Layout>
+  )
 }
 
 export default HomePage
