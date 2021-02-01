394f20e5780ce2341a92906f4205e4b654b31655
Hasan <hsndogn80@gmail.com>
2021-02-01 23:56:39 +0300

    Added tweet modal.

--- components/col-sidebar.js
+++ components/col-sidebar.js
@@ -6,19 +6,26 @@ import styles from './col-sidebar.module.css'
 import Navigaton from './navigation'
 import ThemeButton from './theme-button'
 import ProfileBox from './profile-box'
+import TweetModal from "./tweet-modal";
+import {Tweet} from "./icons";
 
 function SideBar({ flat }) {
+    const [isShowModal, isShowModalSet] = React.useState(false)
   return (
     <div className={cn([styles.sidebar])}>
       <Navigaton flat={flat} />
       <div className={styles.tweet}>
-          <ThemeButton big full={!flat}>
-              {flat ? 'a' : 'Tweet'}
+          <ThemeButton big full={!flat} onClick={() => isShowModalSet(true)}>
+              {flat ? <Tweet/> : 'Tweet'}
           </ThemeButton>
       </div>
+
+        {/*    tweet popup*/}
+        {isShowModal && <TweetModal onClick={() => isShowModalSet(false) }/>}
       <div className={styles.profile}>
         <ProfileBox flat={flat} />
       </div>
+
     </div>
   )
 }
